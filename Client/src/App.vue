8b2fb2fe4672a77b4167cb79c9cb2cd025e56581
Brock Bena <bbena99@gmail.com>
2024-11-13 14:50:49 -0600

    Client init

---
+++ Client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import  NavBar from "./components/NavBar.vue"
+</script>
+
+<template>
+  <div class="w-screen h-screen bg-red-500">
+    <NavBar />
+    <div id="page" class="w-screen bg-purple-500">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#page{
+  height: calc(100vh - 5rem);
+}
+</style>
---
+++ Client/src/components/NavBar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-screen h-20 bg-blue-400">
+  </div>
+</template>
+
+<style scoped>
+</style>
