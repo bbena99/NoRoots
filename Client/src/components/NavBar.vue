7332f427399bad05ed186b528ba32cf2e65f9502
Brock Bena <bbena99@gmail.com>
2024-11-13 21:08:31 -0600

    Added components to the NavBar

--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -1,9 +1,95 @@
 <script setup lang="ts">
+const womansClothes = [
+  'Accessories',
+  'Tops',
+  'Pants',
+  'Dresses'
+];
+const otherProducts = [
+  'candles',
+  'charms'
+]
 </script>
 
 <template>
-  <div class="w-full h-20 bg-bg2">
-    working
+  <nav class="w-full h-20 p-2 bg-bg2 flex items-center text-3xl text-header">
+    <RouterLink to="/" class="font-sans italic">
+      <font-awesome-icon icon="fa-brands fa-pagelines" />
+      No Roots
+    </RouterLink>
+    
+    <button id="dropdownHoverButton" data-dropdown-trigger="hover" data-dropdown-toggle="dropdown1" class="text-text bg-bg2 hover:bg-bg1 focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">
+      <RouterLink to="/products?filter=woman">
+        Woman's clothes
+        <font-awesome-icon icon="fa-solid fa-chevron-down" />
+      </RouterLink>
+    </button>
+    
+    <button id="dropdownDefaultButton" data-dropdown-trigger="hover" data-dropdown-toggle="dropdown" class="text-text bg-bg2 hover:bg-bg1 focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">
+      <RouterLink to="/products?filter=other">
+        Other Products
+        <font-awesome-icon icon="fa-solid fa-chevron-down" />
+      </RouterLink>
+    </button>
+    <!-- Modal toggle -->
+    <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="text-text bg-bg2 hover:bg-bg1 focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center relative right-0" type="button">
+      Login
+      <font-awesome-icon icon="fa-solid fa-user" />
+    </button>
+  </nav>
+  <!-- Dropdown menu -->
+  <div id="dropdown1" class="z-10 hidden bg-bg2 divide-y divide-gray-100 rounded-lg w-44">
+    <ul class="py-2 text-sm text-text" aria-labelledby="dropdownHoverButton">
+      <li v-for="link in womansClothes">
+        <RouterLink :to="'/products?filter='+link" class="block px-4 py-2 hover:bg-bg1">
+          {{ link }}
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+  <!-- Dropdown menu -->
+  <div id="dropdown"  class="z-10 hidden bg-bg2 divide-y divide-gray-100 rounded-lg w-44">
+    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li v-for="link in otherProducts">
+        <RouterLink :to="'/products?filter='+link" class="block px-4 py-2 hover:bg-bg1">
+          {{ link }}
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+  <!-- Main modal -->
+  <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal header -->
+        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            Terms of Service
+          </h3>
+          <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <div class="p-4 md:p-5 space-y-4">
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+          </p>
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+          </p>
+        </div>
+        <!-- Modal footer -->
+        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+          <button data-modal-hide="default-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
